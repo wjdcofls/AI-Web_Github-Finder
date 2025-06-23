27f20bb6ec279dc979eef2a89e59b6ae23707609
Jeong ChaeLin <71626467+wjdcofls@users.noreply.github.com>
2025-06-23 20:08:27 +0900

    github.js 수정

--- github.js
+++ github.js
@@ -8,13 +8,13 @@ class Github {
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}`
     );
 
     console.log(profileResponse);
 
     const repoResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`
     );
 
     const profile = await profileResponse.json();
